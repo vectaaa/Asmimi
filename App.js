3dfc6bd0c2f8bb54e5c17376b5c0472b7fedb229
Vecta <victoronuchukwu16@gmail.com>
2024-08-29 14:30:18 +0100

    How to add items to a lists using useState

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    console.log('new goal', enteredGoalText);
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInputStyle} placeholder='Enter your course goals' onChangeText={goalInputHandler}/>
+        <Button title='Add Goal' onPress={addGoalHandler}/>
+      </View>
+      <View style={styles.goalListContainer}>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    paddingHorizontal: 16
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: '#cccccc'
+  
+  },
+
+  goalListContainer: {
+    flex: 6
+  },
+
+  textInputStyle: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  }
 });
